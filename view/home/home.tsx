3bfbe41bfe60c0aeca68d0982de829c2a47e742f
Joao Pacheco <juhkittopacheco@gmail.com>
2025-01-20 15:44:18 -0300

    Feature: finished

--- view/home/home.tsx
+++ view/home/home.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Image, Text, TouchableOpacity, View, ScrollView } from 'react-native';
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  Alert,
+} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import NetInfo, { NetInfoState } from '@react-native-community/netinfo';
 import * as Location from 'expo-location';
@@ -8,8 +15,6 @@ import AddItemModal from './components/addItemModal/addItemModal';
 import WifiSelectorModal from './components/wifiSelectorModal/wifiSelectorModal';
 import useListStore from '../../view_model/listStore';
 import ListItem from './components/listItem/listItem';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import useWifiStore from '../../view_model/useWifiStore';
 import Toast from 'react-native-toast-message';
 import * as Notifications from 'expo-notifications';
 
@@ -18,14 +23,140 @@ interface Item {
   name: string;
 }
 
+interface LocationType {
+  latitude: number;
+  longitude: number;
+  radius: number;
+}
+
+interface CurrentLocationType {
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
 export default function Home() {
   const [isWifiSelectorModalVisible, setWifiSelectorModalVisible] =
     useState(false);
   const [isAddItemModalVisible, setAddItemModalVisible] = useState(false);
   const scrollViewRef = useRef<ScrollView>(null);
   const { list, addItem, removeItem } = useListStore();
-  const [ssid, setSsid] = useState<string | null>(null);
-  const { savedSsid, saveSsid } = useWifiStore();
+  const [savedLocation, setSavedLocation] = useState<LocationType | null>(null);
+  const [currentLocation, setCurrentLocation] =
+    useState<CurrentLocationType | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [isOutsideRadius, setIsOutsideRadius] = useState<boolean>(false);
+
+  useEffect(() => {
+    let locationSubscription: Location.LocationSubscription | null = null;
+
+    if (savedLocation) {
+      // Inicia o monitoramento de localização
+      const startWatching = async () => {
+        try {
+          locationSubscription = await Location.watchPositionAsync(
+            {
+              accuracy: Location.Accuracy.High,
+              timeInterval: 5000, // Atualiza a cada 5 segundos
+              distanceInterval: 1, // Atualiza a cada metro percorrido
+            },
+            (location) => {
+              setCurrentLocation(location);
+              checkIfOutsideRadius(location);
+            },
+          );
+        } catch (error) {
+          console.error(error);
+          Alert.alert('Erro ao monitorar localização.');
+        }
+      };
+
+      startWatching();
+    }
+
+    return () => {
+      if (locationSubscription) {
+        locationSubscription.remove();
+      }
+    };
+  }, [savedLocation]);
+
+  const deg2rad = (deg: number): number => deg * (Math.PI / 180);
+
+  const calculateDistance = (
+    lat1: number,
+    lon1: number,
+    lat2: number,
+    lon2: number,
+  ): number => {
+    const R = 6371000;
+    const dLat = deg2rad(lat2 - lat1);
+    const dLon = deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) *
+        Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+  };
+
+  const saveCurrentLocationWithRadius = async (): Promise<void> => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permissão para acessar localização foi negada.');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      setSavedLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        radius: 60, // Raio em metros
+      });
+      showToast(
+        'success',
+        'Casa salva com sucesso',
+        'Salvamos um raio de 50 metros da sua casa',
+      );
+      setWifiSelectorModalVisible(false);
+    } catch (error) {
+      console.error(error);
+      showToast('error', 'Casa não salva', 'Não conseguimos salvar sua casa');
+    }
+  };
+
+  const checkIfOutsideRadius = (location: CurrentLocationType): void => {
+    if (!savedLocation) return;
+
+    const distance = calculateDistance(
+      location.coords.latitude,
+      location.coords.longitude,
+      savedLocation.latitude,
+      savedLocation.longitude,
+    );
+
+    if (distance > savedLocation.radius) {
+      if (!isOutsideRadius) {
+        setIsOutsideRadius(true);
+        Notifications.scheduleNotificationAsync({
+          content: {
+            title: 'Lembrex',
+            body: 'Veja os itens que você deve levar com vocé',
+          },
+          trigger: null,
+        });
+      }
+    } else {
+      if (isOutsideRadius) {
+        setIsOutsideRadius(false);
+        Alert.alert('Você voltou para dentro do raio salvo!');
+      }
+    }
+  };
 
   const addNewItem = (newItemName: string) => {
     if (newItemName.trim()) {
@@ -64,56 +195,8 @@ export default function Home() {
     });
   };
 
-  const handleSaveSsid = async () => {
-    if (ssid) {
-      try {
-        await AsyncStorage.setItem('savedWifiSsid', ssid);
-        saveSsid(ssid);
-        showToast('success', 'Wifi salvo com sucesso', 'Wifi: ' + ssid);
-        setWifiSelectorModalVisible(false);
-      } catch (error) {
-        console.error('Erro ao salvar Wifi:', error);
-      }
-    } else {
-      showToast('error', 'Nenhum Wifi conectado', 'Wifi nulo');
-    }
-  };
-
-  // Listener para alterações na conexão
-  const unsubscribe = NetInfo.addEventListener((state) => {
-    console.log('ssid', ssid);
-    console.log('savedSsid', savedSsid);
-    console.log('state.isConnected', state.isConnected);
-    console.log('state.type', state.type);
-
-    if (ssid !== savedSsid) {
-      if (state.isConnected && state.type === 'wifi') {
-        /* Notifications.scheduleNotificationAsync({
-          content: {
-            title: 'Lembrex',
-            body: 'Veja os itens que você deve levar com vocé',
-          },
-          trigger: null,
-        }); */
-      } else {
-        // trocou de wifi para um outro wif
-      }
-    }
-  });
-
   useEffect(() => {
     getLocation();
-
-    NetInfo.configure({
-      shouldFetchWiFiSSID: true,
-    });
-
-    NetInfo.fetch().then((state) => {
-      const details = state.details as WifiDetails;
-      setSsid(details?.ssid || 'Wi-Fi não disponível');
-    });
-
-    unsubscribe();
   }, []);
 
   return (
@@ -174,9 +257,9 @@ export default function Home() {
         onClose={() => setWifiSelectorModalVisible(false)}
         title="Essa é a rede Wi-Fi que você usa em casa?"
         message="O app avisa quando você sai de casa, lembrando dos itens essenciais cadastrados, ao desconectar do Wi-Fi."
-        wifiTitle={ssid || 'Wi-Fi desconectado'}
-        primaryButtonText="ESSE MESMO"
-        onPrimaryButtonPress={() => handleSaveSsid()}
+        wifiTitle={'Você está em casa?'}
+        primaryButtonText="SIM"
+        onPrimaryButtonPress={() => saveCurrentLocationWithRadius()}
         secondaryButtonText="Cancelar"
         onSecondaryButtonPress={() => setWifiSelectorModalVisible(false)}
       />
